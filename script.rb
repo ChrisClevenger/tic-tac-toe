1df9bd691092f43e759085d4f112909689a18de4
ChrisClevenger <aenguscoen@gmail.com>
2023-06-21 15:42:48 -0500

    Outline basic framework in comments

---
+++ script.rb
@@ -0,0 +1,13 @@
+## Create nested array of arrays to form 3x3 grid
+
+
+
+## Create funciton to prompt user to update array by choosing row/column
+
+
+
+## Create methods for checking row, column, and diagonal matches
+
+
+
+## Return the results of the game
